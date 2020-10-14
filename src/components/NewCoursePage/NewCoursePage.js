ea5ad53f4701acca0aa5ce0a751839172ffe3dc5
KStomberg <68118089+KStomberg@users.noreply.github.com>
2020-10-14 10:22:06 -0500

    starting matterial ui setup

--- src/components/NewCoursePage/NewCoursePage.js
+++ src/components/NewCoursePage/NewCoursePage.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import mapStoreToProps from '../../redux/mapStoreToProps';
 
-import './NewCoursePage.css'
+import './NewCoursePage.css';
 
 // Basic class component structure for React with default state
 // value setup. When making a new component be sure to replace
@@ -16,82 +16,153 @@ class NewCoursePage extends Component {
   render() {
     return (
       <div>
+        <fieldset>
+          <div id="createTurnContainer">
+            <div id="turnSeverityRadio">
+              <p class='pDescriptor'>Turn Severity</p>
 
-        <div id='createTurnContainer'>
+              <input
+                type="radio"
+                class="radioStyle"
+                id="severityOne"
+                name="turnSeverity"
+                value="1"
+              />
+              <label htmlFor="severityOne">1</label>
 
-            <div id='turnSeverityRadio'>
+              <input
+                type="radio"
+                class="radioStyle"
+                id="severityTwo"
+                name="turnSeverity"
+                value="2"
+              />
+              <label htmlFor="severityTwo">2</label>
 
-              <p>Turn Severity</p>
+              <input
+                type="radio"
+                class="radioStyle"
+                id="severityThree"
+                name="turnSeverity"
+                value="3"
+              />
+              <label htmlFor="severityThree">3</label>
 
-              <input type='radio' class='radioStyle' id='severityOne' name='turnSeverity' value='1' />
-              <label htmlFor='severityOne'>1</label>
-              
-              <input type='radio' class='radioStyle' id='severityTwo' name='turnSeverity' value='2' />
-              <label htmlFor='severityTwo'>2</label>
+              <input
+                type="radio"
+                class="radioStyle"
+                id="severityFour"
+                name="turnSeverity"
+                value="4"
+              />
+              <label htmlFor="severityFour">4</label>
 
-              <input type='radio' class='radioStyle' id='severityThree' name='turnSeverity' value='3' />
-              <label htmlFor='severityThree'>3</label>
+              <input
+                type="radio"
+                class="radioStyle"
+                id="severityFive"
+                name="turnSeverity"
+                value="5"
+              />
+              <label htmlFor="severityFive">5</label>
 
-              <input type='radio' class='radioStyle' id='severityFour' name='turnSeverity' value='4' />
-              <label htmlFor='severityFour'>4</label>
-
-              <input type='radio' class='radioStyle' id='severityFive' name='turnSeverity' value='5' />
-              <label htmlFor='severityFive'>5</label>
-
-              <input type='radio' class='radioStyle' id='severitySix' name='turnSeverity' value='6' />
-              <label htmlFor='severitySix'>6</label>
-
+              <input
+                type="radio"
+                class="radioStyle"
+                id="severitySix"
+                name="turnSeverity"
+                value="6"
+              />
+              <label htmlFor="severitySix">6</label>
             </div>
 
+            <div id="cutOptionRadio">
+              <p class='pDescriptor'>Cut?</p>
 
-            <div id='cutOptionRadio'>
+              <input
+                type="radio"
+                class="radioStyle"
+                id="selectedCut"
+                name="cutOption"
+                value="cut"
+              />
+              <label htmlFor="selectedCut">Cut</label>
 
-                <p>Cut?</p>
+              <input
+                type="radio"
+                class="radioStyle"
+                id="selectedRegular"
+                name="cutOption"
+                value="regular"
+              />
+              <label htmlFor="selectedRegular">Regular</label>
 
-                <input type='radio' class='radioStyle' id='selectedCut' name='cutOption' value='cut' />
-                <label htmlFor='selectedCut'>Cut</label>
-
-                <input type='radio' class='radioStyle' id='selectedRegular' name='cutOption' value='regular' />
-                <label htmlFor='selectedRegular'>Regular</label>
-
-                <input type='radio' class='radioStyle' id='selectedDontCut' name='cutOption' value='dontCut' />
-                <label htmlFor='selectedDontCut'>Don't Cut</label>
-
+              <input
+                type="radio"
+                class="radioStyle"
+                id="selectedDontCut"
+                name="cutOption"
+                value="dontCut"
+              />
+              <label htmlFor="selectedDontCut">Don't Cut</label>
             </div>
 
-            <div id='intoOptionRadio'>
+            <div id="intoOptionRadio">
+              <p class='pDescriptor'>Into?</p>
 
-              <p>Into?</p>
+              <input
+                type="radio"
+                class="radioStyle"
+                id="selectedInto"
+                name="intoOption"
+                value="into"
+              />
+              <label htmlFor="selectedInto">Into</label>
 
-              <input type='radio' class='radioStyle' id='selectedInto' name='intoOption' value='into' />
-              <label htmlFor='selectedInto'>Into</label>
+              <input
+                type="radio"
+                class="radioStyle"
+                id="selectedThen"
+                name="intoOption"
+                value="Then"
+              />
+              <label htmlFor="selectedThen">Then</label>
 
-              <input type='radio' class='radioStyle' id='selectedThen' name='intoOption' value='Then' />
-              <label htmlFor='selectedThen'>Then</label>
-
-              <input type='radio' class='radioStyle' id='selectedAnd' name='intoOption' value='and' />
-              <label htmlFor='selectedAnd'>And</label>
-
+              <input
+                type="radio"
+                class="radioStyle"
+                id="selectedAnd"
+                name="intoOption"
+                value="and"
+              />
+              <label htmlFor="selectedAnd">And</label>
             </div>
 
-            <div id='directionOptionRadio'>
+            <div id="directionOptionRadio">
+              <p class='pDescriptor'>Direction?</p>
 
-              <p>Direction?</p>
+              <input
+                type="radio"
+                class="radioStyle"
+                id="selectedLeft"
+                name="directionOption"
+                value="left"
+              />
+              <label htmlFor="selectedLeft">Left</label>
 
-              <input type='radio' class='radioStyle' id='selectedLeft' name='directionOption' value='left' />
-              <label htmlFor='selectedLeft'>Left</label>
-
-              <input type='radio' class='radioStyle' id='selectedRight' name='directionOption' value='right' />
-              <label htmlFor='selectedRight'>Right</label>
-
+              <input
+                type="radio"
+                class="radioStyle"
+                id="selectedRight"
+                name="directionOption"
+                value="right"
+              />
+              <label htmlFor="selectedRight">Right</label>
             </div>
+          </div>
+        </fieldset>
 
-        </div>
-
-        <div id='miscOptionsContainer'>
-
-        </div>
-
+        <div id="miscOptionsContainer"></div>
       </div>
     );
   }
