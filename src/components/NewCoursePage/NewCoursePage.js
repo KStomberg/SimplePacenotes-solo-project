3566d8c8af9da8a6f135bd997d439a807bcc3580
KStomberg <68118089+KStomberg@users.noreply.github.com>
2020-10-14 15:49:19 -0500

    note and distance updates state on change

--- src/components/NewCoursePage/NewCoursePage.js
+++ src/components/NewCoursePage/NewCoursePage.js
@@ -11,115 +11,150 @@ import TextField from '@material-ui/core/TextField';
 import ToggleButton from '@material-ui/lab/ToggleButton';
 import ToggleButtonGroup from '@material-ui/lab/ToggleButtonGroup';
 
-
 import './NewCoursePage.css';
 
-
-
 class NewCoursePage extends Component {
-
-  state= {
+  state = {
     turnSeverity: '',
     into: '',
     cut: '',
-    direction: ''
-  }
+    direction: '',
+    jump: false,
+    loose: false,
+    caution: false,
+    flat: false,
+    distance: '',
+    note: '',
+  };
 
   onChangeHandler = (event, propertyName) => {
     console.log('Changing:', propertyName, event.target.value);
     this.setState({
       ...this.state,
-      [propertyName]: event.target.value
-    })
-  }
+      [propertyName]: event.target.value,
+    });
+  };
 
   render() {
-    console.log('this.state:', this.state)
+    console.log('this.state:', this.state);
     return (
-      
       <div>
-        <Grid container spacing={2} direction="column" alignItems="center">
-          <Grid item>
-            <p>Turn Severity</p>
-            <ToggleButtonGroup size="medium" value={this.state.turnSeverity} exclusive onChange={(event) => this.onChangeHandler(event, 'turnSeverity')} >
-              <ToggleButton value="1">
-                <label>1</label>
-              </ToggleButton>
-              <ToggleButton value="2">
-                <label>2</label>
-              </ToggleButton>
-              <ToggleButton value="3">
-                <label>3</label>
-              </ToggleButton>
-              <ToggleButton value="4">
-                <label>4</label>
-              </ToggleButton>
-              <ToggleButton value="5">
-                <label>5</label>
-              </ToggleButton>
-              <ToggleButton value="6">
-                <label>6</label>
-              </ToggleButton>
-            </ToggleButtonGroup>
-          </Grid>
-        
+        <div id="createTurnContainer">
+          <Grid container spacing={2} direction="column" alignItems="center">
+            <Grid item>
+              <p>Turn Severity</p>
+              <ToggleButtonGroup
+                size="medium"
+                value={this.state.turnSeverity}
+                exclusive
+                onChange={(event) =>
+                  this.onChangeHandler(event, 'turnSeverity')
+                }
+              >
+                <ToggleButton value="1">
+                  <label>1</label>
+                </ToggleButton>
+                <ToggleButton value="2">
+                  <label>2</label>
+                </ToggleButton>
+                <ToggleButton value="3">
+                  <label>3</label>
+                </ToggleButton>
+                <ToggleButton value="4">
+                  <label>4</label>
+                </ToggleButton>
+                <ToggleButton value="5">
+                  <label>5</label>
+                </ToggleButton>
+                <ToggleButton value="6">
+                  <label>6</label>
+                </ToggleButton>
+              </ToggleButtonGroup>
+            </Grid>
 
-        
-          <Grid item>
-            <p>Into?</p>
-            <ToggleButtonGroup size="medium" value={this.state.into} exclusive onChange={(event) => this.onChangeHandler(event, 'into')}>
-              <ToggleButton value="into">
-                <label>Into</label>
-              </ToggleButton>
-              <ToggleButton value="then">
-                <label>Then</label>
-              </ToggleButton>
-              <ToggleButton value="and">
-                <label>And</label>
-              </ToggleButton>
-            </ToggleButtonGroup>
-          </Grid>
-        
+            <Grid item>
+              <p>Into?</p>
+              <ToggleButtonGroup
+                size="medium"
+                value={this.state.into}
+                exclusive
+                onChange={(event) => this.onChangeHandler(event, 'into')}
+              >
+                <ToggleButton value="into">
+                  <label>Into</label>
+                </ToggleButton>
+                <ToggleButton value="then">
+                  <label>Then</label>
+                </ToggleButton>
+                <ToggleButton value="and">
+                  <label>And</label>
+                </ToggleButton>
+              </ToggleButtonGroup>
+            </Grid>
 
-        
-          <Grid item>
-            <p>Cut?</p>
-            <ToggleButtonGroup size="medium" value={this.state.cut} exclusive onChange={(event) => this.onChangeHandler(event, 'cut')}>
-              <ToggleButton value="cut">
-                <label>Cut</label>
-              </ToggleButton>
-              <ToggleButton value="dontCut">
-                <label>Don't Cut</label>
-              </ToggleButton>
-            </ToggleButtonGroup>
-          </Grid>
-     
+            <Grid item>
+              <p>Cut?</p>
+              <ToggleButtonGroup
+                size="medium"
+                value={this.state.cut}
+                exclusive
+                onChange={(event) => this.onChangeHandler(event, 'cut')}
+              >
+                <ToggleButton value="cut">
+                  <label>Cut</label>
+                </ToggleButton>
+                <ToggleButton value="dontCut">
+                  <label>Don't Cut</label>
+                </ToggleButton>
+              </ToggleButtonGroup>
+            </Grid>
 
-        
-          <Grid item>
-            <p>Direction</p>
-            <ToggleButtonGroup size="medium" value={this.state.direction} exclusive onChange={(event) => this.onChangeHandler(event, 'direction')}>
-              <ToggleButton value="left">
-                <label>Left</label>
-              </ToggleButton>
-              <ToggleButton value="right">
-                <label>Right</label>
-              </ToggleButton>
-            </ToggleButtonGroup>
+            <Grid item>
+              <p>Direction</p>
+              <ToggleButtonGroup
+                size="medium"
+                value={this.state.direction}
+                exclusive
+                onChange={(event) => this.onChangeHandler(event, 'direction')}
+              >
+                <ToggleButton value="left">
+                  <label>Left</label>
+                </ToggleButton>
+                <ToggleButton value="right">
+                  <label>Right</label>
+                </ToggleButton>
+              </ToggleButtonGroup>
+            </Grid>
           </Grid>
-        </Grid>
+        </div>
 
         <div id="miscOptionsContainer">
-            <Button variant='contained'>Jump</Button>
-            <Button variant='contained'>Loose/Slippy</Button>
-            <br></br>
-            <Button variant='contained'>Caution</Button>
-            <Button variant='contained'>Flat</Button>
-            <br></br>
-            <TextField id='distance' label='Distance (Meters)' variant='outlined' />
-            <br></br>
-            <TextField id='note' label='Notes' placeholder='You can come back to this later' multiline rows={4} variant='outlined' />
-            
+          <p>Misc Options</p>
+          <Button variant="contained">Jump</Button>
+          <Button variant="contained">Loose/Slippy</Button>
+          <br></br>
+          <Button variant="contained">Caution</Button>
+          <Button variant="contained">Flat</Button>
+          <br></br>
+          <TextField
+            id="distance"
+            label="Distance (Meters)"
+            variant="outlined"
+            type="number"
+            value={this.state.distance}
+            onChange={(event) => this.onChangeHandler(event, 'distance')}
+          />
+          <br></br>
+          <TextField
+            id="note"
+            label="Notes"
+            placeholder="You can come back to this later"
+            multiline
+            rows={4}
+            variant="outlined"
+            value={this.state.note}
+            onChange={(event) => this.onChangeHandler(event, 'note')}
+          />
         </div>
       </div>
     );
