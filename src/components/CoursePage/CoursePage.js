dd690077c62454542258ef4f6e753751e6f1c44f
KStomberg <68118089+KStomberg@users.noreply.github.com>
2020-10-16 09:50:33 -0500

    addCourse button added

--- src/components/CoursePage/CoursePage.js
+++ src/components/CoursePage/CoursePage.js
@@ -2,11 +2,18 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import mapStoreToProps from '../../redux/mapStoreToProps';
 
+//Material-UI
+import Button from '@material-ui/core/Button';
+
 import CourseItem from '../CourseItem/CourseItem'
 import './CoursePage.css'
 
 
 class CoursePage extends Component {
+
+    state= {
+        newCourse: ''
+    }
 
   componentDidMount() {
       this.getCourse();
@@ -20,11 +27,24 @@ class CoursePage extends Component {
       });
   }
 
+  addCourse = () => {
+      console.log('addCourse button clicked');
+
+      this.props.dispatch({
+          type: 'CREATE_COURSE',
+          payload: this.state
+      })
+      this.setState({
+        newCourse: ''
+      });
+  }
+
   render() {
       console.log('json stringify', this.props.store.course.courseReducer);
     return (
       <div class='coursePage'>
         <h2>List of all courses:</h2>
+        <Button variant='contained' onClick={this.addCourse} > New Course </Button>
         {this.props.store.course.courseReducer.map((course) => (
             <CourseItem key={course.id} course={course} />
         ))}
